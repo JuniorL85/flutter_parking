27b82df3b4b009519c9d20eb5f43c53c624028ca
Junior Lopes <albertino-junior.lopes-livramento@vgregion.se>
2024-11-14 14:32:36 +0100

    lite fixar

--- parking_user/lib/widgets/login.dart
+++ parking_user/lib/widgets/login.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  Login({super.key});
+  final TextEditingController _inputController = TextEditingController();
+
+  void dispose() {
+    _inputController.dispose();
+    // super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +16,30 @@ class Login extends StatelessWidget {
         title: const Text('ParkHere'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
+      body: Center(
         child: Card(
           child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            SizedBox(
-              width: 250,
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Personnummer',
-                ),
+            const Text('Logga in', style: TextStyle(fontSize: 30)),
+            const SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              controller: _inputController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Ange personnummer',
               ),
             ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text('Disabled'),
-            ),
+            const SizedBox(height: 30),
+            ValueListenableBuilder<TextEditingValue>(
+              valueListenable: _inputController,
+              builder: (context, value, child) {
+                return ElevatedButton(
+                  onPressed: value.text.isNotEmpty ? () {} : null,
+                  child: const Text('Logga in'),
+                );
+              },
+            )
           ]),
         ),
       ),
